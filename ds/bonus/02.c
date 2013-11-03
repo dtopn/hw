06b9c875d9304559602a64b38a9e88b086658e5c
Tao J <tao-j@outlook.com>
2013-11-03 19:46:06 +0800

    Small altenation

--- ds/bonus/02.c
+++ ds/bonus/02.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,50 +13,52 @@ int main() {
 	//cin >> N;
 	scanf("%d", &N);
 	n = N;
-	while(n) {
-	int *result, *heap, *test;
-	result = (int *) malloc (n * sizeof(int));
-	heap = (int *) malloc (n * sizeof(int));
-	test = (int *) malloc (n * sizeof(int));
-	memset(test, -1, n * sizeof(int));
-	int temp;
-	int i;
-	for (i = 0; i < N; i++) {
-		//cin >> temp;
-		scanf("%d", &temp);
-		*(result + i) = temp;
-	}
-	memcpy(heap, result, n * sizeof(int));
-	sort(heap, heap + n);
-	//space reclaimation is ignored in this program
-	int poped = 0;
-	int *tmp, posi;
-	tmp = heap;
-	while(*heap == -1)
-		heap++, poped++, tmp = heap;
-	while (poped != n) {
-		tmp = heap;
-		if(*heap == -1)
-			heap++, tmp = heap;
-		else {
-			while (result[posi = hash(*tmp, n, test)] != *tmp) {
-				do {
-					tmp++;
-				}
-				while (*tmp == -1);
-			}
-			ins(*tmp, posi, test);
-			if (poped != n - 1)
-			//cout << *tmp << ' ';
-			printf("%d%c", *tmp, ' ');
-			else
-				//cout << *tmp << endl;
-				printf("%d%c", *tmp, '\n');
-			*tmp = -1; poped++;
+	while (n) {
+		int *result, *heap, *test;
+		int *heap_c;
+		result = (int *)malloc((n + 1) * sizeof(int));
+		heap = (int *)malloc((n + 1) * sizeof(int)); heap_c = heap;
+		test = (int *)malloc((n + 1)* sizeof(int));
+		memset(test, -1, (n + 1) * sizeof(int));
+		test[n] = -2;
+		int temp;
+		int i;
+		for (i = 0; i < N; i++) {
+			//cin >> temp;
+			scanf("%d", &temp);
+			*(result + i) = temp;
 		}
-	}
-	scanf("%d", &N);
-	n = N;
+		memcpy(heap, result, (n + 1) * sizeof(int));
+		sort(heap, heap + n);
+		//space reclaimation is ignored in this program
+		int poped = 0;
+		int *tmp, posi;
+		tmp = heap;
+		while (*heap == -1)
+			heap++, poped++, tmp = heap;
+		while (poped != n) {
+			tmp = heap;
+			if (*heap == -1)
+				heap++, tmp = heap;
+			else {
+				while (result[posi = hash(*tmp, n, test)] != *tmp) {
+					do {
+						tmp++;
+					} while (*tmp == -1);
+				}
+				ins(*tmp, posi, test);
+				if (poped != n - 1)
+					//cout << *tmp << ' ';
+					printf("%d%c", *tmp, ' ');
+				else
+					//cout << *tmp << endl;
+					printf("%d%c", *tmp, '\n');
+				*tmp = -1; poped++;
+			}
+		}
+		scanf("%d", &N);
+		n = N;
+		free(result); free(heap_c); free(test);
 	}
 	return 0;
 }
